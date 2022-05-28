80de6d2c12fe5a1ea9a075211735ef98dc7afbb9
leuys <34769041+leuys@users.noreply.github.com>
2022-05-28 08:46:37 +0000

    border node info

--- src/components/stage/Graph.vue
+++ src/components/stage/Graph.vue
@@ -268,7 +268,7 @@ export default {
   left: 50%;
   transform: translate(-50%, 110%);
   transition:transform .4s ease;
-  box-shadow: 0 0 1em rgba(0,0,0,.25);
+  border: var(--main-border);
   display: flex;
   justify-content: space-between;
   align-items: center;
