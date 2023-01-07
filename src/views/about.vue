af6c985eac8e9a1e5d1ea56d7b34e15289c73cfc
leuys <34769041+leuys@users.noreply.github.com>
2023-01-07 10:48:54 +0000

    new about

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,15 +1,38 @@
 <template>
+	
 	<router-link to="/" class="single_header_nav_link">Create your hyperchannel</router-link>
+	
 	<section class="page">
+		
 		<h1>ABOUT</h1>
-		<p>Hyperchannel is an experimental tool centered around new ways of visualizing and connecting knowledge in digital space.</p>
-		<p>Created by <a href="https://joschabruening.eu/" target="_blank">Joscha Brüning</a> and <a href="https://leuys.com" target="_blank">Benjamin Unterluggauer</a>.</p>
+		
 		<p>
-			Hyperchannel makes use of the <a href="https://are.na" target="_blank">Are.na</a> API. 
-			The 3D space is rendered via the <a href="https://threejs.org" target="_blank">three.JS</a> library and uses 
-			<a href="https://www.google.com/search?client=safari&rls=en&q=d3+force+graph&ie=UTF-8&oe=UTF-8" target="_blank">D3 Force-Directed Graph</a> layout,
-			together with the <a href="https://github.com/vasturiano/3d-force-graph" target="_blank">3d-force-graph</a> by Vasco Asturiano.
+			Hyperchannel is an (experimental) graphing tool to assemble and connect data in virtual space.
+			It makes use of the <a href="https://are.na" target="_blank">Are.na</a> API and lets users create 
+			a three-dimensional tapestry of their Are.na blocks.
 		</p>
-		<p>v 0.2</p>
+		
+		<p>
+			3D space is rendered via <a href="https://threejs.org" target="_blank">three.JS</a> and uses a
+			<a href="https://observablehq.com/@d3/force-directed-graph" target="_blank">D3 Force-Directed Graph</a> layout,
+			together with the <a href="https://github.com/vasturiano/3d-force-graph" target="_blank">3d-force-graph</a> code by Vasco Asturiano.
+			UI build with <a href="https://vuejs.org" target="_blank">Vue</a>. 
+		</p>
+		
+		<p>Created by <a href="https://joschabruening.eu/" target="_blank">Joscha Brüning</a> and <a href="https://leuys.com" target="_blank">Benjamin Unterluggauer</a> in early 2022.</p>
+		
+		<p><small>(Last Updated 01.2023) v 0.25</small></p>
+		
+		<IntroVideo />
+		
 	</section>
+
 </template>
+
+<script>
+import IntroVideo from '@/components/setup/IntroVideo.vue'
+	
+export default {
+	components: { IntroVideo },
+}
+</script>
